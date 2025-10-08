40bc24cbc49608ebe48fe5d81c8b07e908dcf661
Waquinn <maekowakinn@gmail.com>
2025-10-08 12:45:14 +0800

    Add files via upload

---
+++ Contents/content.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class content {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("\n\n\nWelcome to Mini Calculator!");
+        System.out.println("[1] Add");
+        System.out.println("[2] Subtract");
+        System.out.println("[3] Multiply");
+        System.out.println("[4] Divide");
+        System.out.println("[5] Exit");
+        System.out.print("What do you want to do?: ");
+        int choice = in.nextInt();
+        System.out.print("Enter your 1st number: ");
+        int num1 = in.nextInt();
+        System.out.print("Enter your 2nd number: ");
+        int num2 = in.nextInt();
+        System.out.println("I MISS YOU :<<");
+    }
+}
